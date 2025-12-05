32ba5a04cc5b4f29ddc3f77d5ae44d9f3f8fd267
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2025-12-05 09:14:32 +0100

    adding google fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const home = () => {
   return (
     <div>
-      <h1 className="flex justify-center text-4xl text-red-300">
+      <h1 className="font-space-grotesk flex justify-center text-4xl text-red-300">
         hello world👋
       </h1>
       <p className="text-red-400">this is my first next.js react app</p>
